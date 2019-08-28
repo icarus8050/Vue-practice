6bf4db533b006f0b873ab32a78d0f600c6e00655
icarus8050 <icarus8050@naver.com>
2019-08-28 23:47:06 +0900

    action button

--- TheMonsterSlayer/app.js
+++ TheMonsterSlayer/app.js
@@ -13,22 +13,35 @@ new Vue({
         },
         attack: function() {
             this.monsterHealth -= this.calculateDamage(3, 10);
-
             if (this.checkWin()) {
                 return;
             }
 
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+            this.monsterAttacks();
         },
         specialAttack: function() {
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            if (this.checkWin()) {
+                return;
+            }
 
+            this.monsterAttacks();
         },
         heal: function() {
-
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.playerHealth += 10;
+            this.monsterAttacks();
         },
         giveUp: function() {
-
+            this.gameIsRunning = false;
+        },
+        monsterAttacks: function() {
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -42,13 +55,14 @@ new Vue({
                 }
                 return true;
             } else if (this.playerHealth <= 0) {
-                if (confime('You lost! New Game?')) {
+                if (confirm('You lost! New Game?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
                 }
-                return false;
+                return true;
             }
+            return false;
         }
-    },
-})
+    }
+});
