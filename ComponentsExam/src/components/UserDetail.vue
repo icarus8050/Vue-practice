bdfe490026c30d48b20022291cdb62a5fd18d7d8
icarus8050 <icarus8050@naver.com>
2019-09-07 14:44:18 +0900

    eventBus exam

--- ComponentsExam/src/components/UserDetail.vue
+++ ComponentsExam/src/components/UserDetail.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+    import {eventBus} from '../main'
+
     export default {
         props: {
             myName: {
@@ -31,6 +33,11 @@
                 this.myName = 'Leafy';
                 this.$emit('nameWasReset', this.myName);
             }
+        },
+        created() {
+            eventBus.$on('ageWasEdited', (age) => {
+                this.userAge = age;
+            });
         }
     }
 </script>
