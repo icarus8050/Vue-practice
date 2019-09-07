e44ca2ac80e85f9e936346a123c56d795795df2a
icarus8050 <icarus8050@naver.com>
2019-09-07 12:09:11 +0900

    Communicating between sibling components

--- ComponentsExam/src/components/UserDetail.vue
+++ ComponentsExam/src/components/UserDetail.vue
@@ -3,7 +3,9 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name : {{ switchName() }}</p>
+        <p>User Age : {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
     </div>
 </template>
 
@@ -17,7 +19,9 @@
                         name: 'Max'
                     }
                 }
-            }
+            },
+            resetFn: Function,
+            userAge: Number
         },
         methods: {
             switchName() {
