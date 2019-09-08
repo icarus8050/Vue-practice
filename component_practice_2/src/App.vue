979f4e3aed6973832a8fb0de8e7453f1c20fed2c
icarus8050 <icarus8050@naver.com>
2019-09-08 22:48:43 +0900

    default slot exam

--- component_practice_2/src/App.vue
+++ component_practice_2/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12">
                <app-quote>
-                   <h2>The Quote</h2>
+                   <h2 slot="title">{{ quoteTitle }}</h2>
                    <p>A wonderful Quote</p>
                </app-quote>
             </div>
@@ -16,11 +16,16 @@
     import Quote from "./components/Quote.vue";
 
     export default {
+        data: function() {
+            return {
+                quoteTitle: 'The Quote'
+            }
+        },
         components: {
             appQuote: Quote
         }
     }
 </script>
 
-<style>
+<style scoped>
 </style>
