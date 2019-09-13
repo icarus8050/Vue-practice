8d4ba89570f61fb9e983c89505437de3bec9f11c
icarus8050 <icarus8050@naver.com>
2019-09-13 14:56:01 +0900

    practice custom directive

--- directive_practice_2/src/App.vue
+++ directive_practice_2/src/App.vue
@@ -5,14 +5,34 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <p v-myon:click="clicked" class="btn btn-primary">Click me!!</p>
+                <hr>
+                <span class="row">{{ count }}</span>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data: function() {
+            return {
+                count: 0
+            }
+        },
+        directives: {
+            myon: {
+                bind(el, binding) {
+                    el.addEventListener(binding.arg, binding.value);
+                }
+            }
+        },
+        methods: {
+            clicked() {
+                this.count++;
+            }
+        }
+    }
 </script>
 
 <style>
