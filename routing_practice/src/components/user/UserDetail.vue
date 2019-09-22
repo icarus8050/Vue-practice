277c49b2a465d0f7c17676bcf86fcf5d5bcc714a
icarus8050 <icarus8050@naver.com>
2019-09-22 12:30:59 +0900

    controlling the scroll behavior

--- routing_practice/src/components/user/UserDetail.vue
+++ routing_practice/src/components/user/UserDetail.vue
@@ -4,7 +4,28 @@
         <p>User loaded has ID : {{ $route.params.id }}</p>
         <router-link
                 tag="button"
-                :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
-                class="btn btn-primary">Edit User</router-link>
+                :to="link"
+                class="btn btn-primary">Edit User
+        </router-link>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        locale: 'en',
+                        q: 100
+                    },
+                    hash: '#data'
+                }
+            }
+        }
+    }
+</script>
